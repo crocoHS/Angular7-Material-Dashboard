7ae8569f22a8a79ea3278a5f49995993ef42fb22
crocoHS <crocoHS.1205@gmail.com>
2019-05-10 23:17:13 +0700

    fix option-dropdown

--- src/app/shared/components/option-dropdown/option-dropdown.component.ts
+++ src/app/shared/components/option-dropdown/option-dropdown.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-option-dropdown',
-  templateUrl: './option-dropdown.component.html',
-  styleUrls: ['./option-dropdown.component.scss']
+    selector: 'app-option-dropdown',
+    templateUrl: './option-dropdown.component.html',
+    styleUrls: ['./option-dropdown.component.scss']
 })
 export class OptionDropdownComponent implements OnInit {
+    public isShowDropDown = false;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    showDropdown() {
+        this.isShowDropDown = !this.isShowDropDown;
+        console.log('cok', this.isShowDropDown);
+    }
+
+    ngOnInit() {
+    }
 
 }
