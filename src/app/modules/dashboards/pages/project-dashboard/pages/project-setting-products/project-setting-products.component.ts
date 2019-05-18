33c533ea8f8298db97639aa1d4ef5a07e62974d9
crocoHS <crocoHS.1205@gmail.com>
2019-05-18 09:06:06 +0700

    add products setting

--- src/app/modules/dashboards/pages/project-dashboard/pages/project-setting-products/project-setting-products.component.ts
+++ src/app/modules/dashboards/pages/project-dashboard/pages/project-setting-products/project-setting-products.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, ValidationErrors, Validators } from '@angular/forms';
+import { untilDestroyed } from 'ngx-take-until-destroy';
 
 @Component( {
     selector: 'app-project-setting-products',
     templateUrl: './project-setting-products.component.html',
     styleUrls: [ './project-setting-products.component.scss' ]
 } )
-export class ProjectSettingProductsComponent implements OnInit {
+export class ProjectSettingProductsComponent implements OnInit, OnDestroy {
     // TODO: KEPIKIRIAN TERUS CARA NGGAWE CATEGORY
 
     // FORM GROUP Product
@@ -16,12 +17,21 @@ export class ProjectSettingProductsComponent implements OnInit {
         price: [ '', [ Validators.required, Validators.pattern( '^[0-9]+$' ) ] ],
         minPrice: [ '' ],
     } );
+
     // Data Product Image
     public productImage: string[];
     // for Carousel
     public carouselImage = 0;
 
     constructor( private fb: FormBuilder ) {
+    }
+
+    changeMinPrice() {
+        const price = this.formGroup.get( 'price' ).value;
+        const minPrice = this.formGroup.get( 'minPrice' ).value;
+        if ( price <= minPrice) {
+            this.formGroup.get( 'minPrice' ).setValue( price );
+        }
     }
 
     changeImage( index: number ) {
@@ -35,4 +45,6 @@ export class ProjectSettingProductsComponent implements OnInit {
                 'https://dummyimage.com/600x400/000/fff.jpg', 'https://dummyimage.com/1024x600/000/fff.jpg' ];
     }
 
+    ngOnDestroy(): void {
+    }
 }
