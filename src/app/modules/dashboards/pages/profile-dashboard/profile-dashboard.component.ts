428daf152410e1d30d32650cb728ac48dd1dc4e6
nikenarra0801 <nikenarra0801@gmail.com>
2019-07-08 19:30:08 +0700

    add user profile

--- src/app/modules/dashboards/pages/profile-dashboard/profile-dashboard.component.ts
+++ src/app/modules/dashboards/pages/profile-dashboard/profile-dashboard.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { AuthGuardService } from 'src/app/core/guards/auth-guard.service';
 @Component({
   selector: 'app-profile-dashboard',
   templateUrl: './profile-dashboard.component.html',
   styleUrls: ['./profile-dashboard.component.scss']
 })
-export class ProfileDashboardComponent implements OnInit {
+export class ProfileDashboardComponent {
 
-  constructor() { }
+  form = new FormGroup ({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    file: new FormControl('',Validators.required),
+    name: new FormControl('',Validators.required),
+    address: new FormControl('',Validators.required),
+    number: new FormControl('',Validators.required),
+    pic: new FormControl('',Validators.required),
+    password: new FormControl('',Validators.required),
+});
 
-  ngOnInit() {
+  get file(){
+    return this.form.value;
   }
 
+  get email(){
+    return this.form.get('email');
+  }
+
+  get password(){
+    return this.form.get('password');
+  }
+
+  savechanges(){
+    this.form.setErrors({
+      invalidSavechanges: true 
+    });
+  }
 }
